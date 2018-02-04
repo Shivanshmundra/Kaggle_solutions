de6d8dd97f5aee05864c10b1667e9074ec7e4e8b
Unknown <shivanshmundra1@gmail.com>
2018-02-04 17:38:57 +0530

    tensorflow added

--- titanic/code.py
+++ titanic/code.py
@@ -2,19 +2,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC, LinearSVC
+import tensorflow as tf
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import Perceptron
-from sklearn.linear_model import SGDClassifier
-from sklearn.tree import DecisionTreeClassifier
+from collections import namedtuple
+
 
 
 df1 = pd.read_csv('train.csv')
 train_df = df1.copy()
-test_df = pd.read_csv('train.csv')
+test_df = pd.read_csv('test.csv')
 
 # print("Before", train_df.shape, test_df.shape)
 train_df = train_df.drop(['Ticket', 'Cabin'], axis =1)
@@ -77,7 +73,7 @@ for dataset in combine:
     dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
     dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
     dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
-    dataset.loc[ dataset['Age'] > 64, 'Age']
+    dataset.loc[ dataset['Age'] > 64, 'Age'] = 4
 
 
 train_df = train_df.drop(['AgeBand'], axis = 1)
@@ -124,22 +120,155 @@ train_df = train_df.drop(['FareBand'], axis=1)
 combine = [train_df, test_df]
 
 
-X_train = train_df.drop("Survived", axis=1)
+# X_train = train_df.drop("Survived", axis=1)
+X_train = train_df
 Y_train = train_df["Survived"]
-X_test  = test_df.drop(["PassengerId", "Survived"], axis=1).copy()
+X_test  = test_df.drop(["PassengerId"], axis=1).copy()
 #print(X_train.shape, Y_train.shape, X_test.shape)
 
+def dummy_data(data, columns):
+    for column in columns:
+        data = pd.concat([data, pd.get_dummies(data[column], prefix=column)], axis=1)
+        data = data.drop(column, axis=1)
+    return data
 
-random_forest = RandomForestClassifier(n_estimators=100)
-random_forest.fit(X_train, Y_train)
-Y_pred = random_forest.predict(X_test)
-random_forest.score(X_train, Y_train)
-acc_random_forest = round(random_forest.score(X_train, Y_train) * 100, 2)
-print(acc_random_forest)
 
-submission = pd.DataFrame({
-        "PassengerId": test_df["PassengerId"],
-        "Survived": Y_pred
-    })
-submission.to_csv('submission.csv', index=False)
+dummy_columns = ["Pclass", "Sex", "Age*Class", "IsAlone", "Title", "Embarked", "Fare", "Age"]
+train_data=dummy_data(X_train, dummy_columns)
+test_data=dummy_data(X_test, dummy_columns)
+print(train_data.head(10))
+
+
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+
+def split_valid_test_data(data, fraction=(1 - 0.8)):
+    data_y = data["Survived"]
+    lb = LabelBinarizer()
+    data_y = lb.fit_transform(data_y)
+
+    data_x = data.drop(["Survived"], axis=1)
+
+    train_x, valid_x, train_y, valid_y = train_test_split(data_x, data_y, test_size=fraction)
+
+    return train_x.values, train_y, valid_x, valid_y
+
+train_x, train_y, valid_x, valid_y = split_valid_test_data(train_data)
+
+
+
+#print(X_train.head())
+
+# random_forest = RandomForestClassifier(n_estimators=500)
+# random_forest.fit(X_train, Y_train)
+# Y_pred = random_forest.predict(X_test)
+# random_forest.score(X_train, Y_train)
+# acc_random_forest = round(random_forest.score(X_train, Y_train) * 100, 2)
+# print(acc_random_forest)
+
+
+def build_neural_network(hidden_units1=80, hidden_units2=40):
+    tf.reset_default_graph()
+    inputs = tf.placeholder(tf.float32, shape=[None, train_x.shape[1]])
+    labels = tf.placeholder(tf.float32, shape=[None, 1])
+    learning_rate = tf.placeholder(tf.float32)
+    is_training=tf.Variable(True,dtype=tf.bool)
+    
+    initializer = tf.contrib.layers.xavier_initializer()
+    fc = tf.layers.dense(inputs, hidden_units1, activation=None,kernel_initializer=initializer)
+    fc=tf.layers.batch_normalization(fc, training=is_training)
+    fc=tf.nn.relu(fc)
+
+    # fc1 = tf.layers.dense(hidden_units1, hidden_units2, activation=None, kernel_initializer=initializer)
+    # fc1 = tf.layers.batch_normalization(fc1, training=is_training)
+    # fc1 = tf.nn.relu(fc1)
+    
+    logits = tf.layers.dense(fc, 1, activation=None)
+    cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=labels, logits=logits)
+    cost = tf.reduce_mean(cross_entropy)
+    
+    with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    predicted = tf.nn.sigmoid(logits)
+    correct_pred = tf.equal(tf.round(predicted), labels)
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+    # Export the nodes 
+    export_nodes = ['inputs', 'labels', 'learning_rate','is_training', 'logits',
+                    'cost', 'optimizer', 'predicted', 'accuracy']
+    Graph = namedtuple('Graph', export_nodes)
+    local_dict = locals()
+    graph = Graph(*[local_dict[each] for each in export_nodes])
+
+    return graph
+
+model = build_neural_network()
+
+
+def get_batch(data_x,data_y,batch_size=32):
+    batch_n=len(data_x)//batch_size
+    for i in range(batch_n):
+        batch_x=data_x[i*batch_size:(i+1)*batch_size]
+        batch_y=data_y[i*batch_size:(i+1)*batch_size]
+        
+        yield batch_x,batch_y
+epochs = 150
+train_collect = 50
+train_print=train_collect*2
+
+learning_rate_value = 0.0005
+batch_size=16
+
+x_collect = []
+train_loss_collect = []
+train_acc_collect = []
+valid_loss_collect = []
+valid_acc_collect = []
+
+saver = tf.train.Saver()
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    iteration=0
+    for e in range(epochs):
+        for batch_x,batch_y in get_batch(train_x,train_y,batch_size):
+            iteration+=1
+            feed = {model.inputs: train_x,
+                    model.labels: train_y,
+                    model.learning_rate: learning_rate_value,
+                    model.is_training:True
+                   }
+
+            train_loss, _, train_acc = sess.run([model.cost, model.optimizer, model.accuracy], feed_dict=feed)
+            
+            if iteration % train_collect == 0:
+                x_collect.append(e)
+                train_loss_collect.append(train_loss)
+                train_acc_collect.append(train_acc)
+
+                if iteration % train_print==0:
+                     print("Epoch: {}/{}".format(e + 1, epochs),
+                      "Train Loss: {:.4f}".format(train_loss),
+                      "Train Acc: {:.4f}".format(train_acc))
+                        
+                feed = {model.inputs: valid_x,
+                        model.labels: valid_y,
+                        model.is_training:False
+                       }
+                val_loss, val_acc = sess.run([model.cost, model.accuracy], feed_dict=feed)
+                valid_loss_collect.append(val_loss)
+                valid_acc_collect.append(val_acc)
+                
+                if iteration % train_print==0:
+                    print("Epoch: {}/{}".format(e + 1, epochs),
+                      "Validation Loss: {:.4f}".format(val_loss),
+                      "Validation Acc: {:.4f}".format(val_acc))
+    saver.save(sess, "./titanic.ckpt")
+
+
+# submission = pd.DataFrame({
+#         "PassengerId": test_df["PassengerId"],
+#         "Survived": Y_pred
+#     })
+# submission.to_csv('submission.csv', index=False)
 
